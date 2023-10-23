19f86c5302f1b8c2e1b9cf3be7add890d3eea98c
Comandosss <cotum@mail.ru>
2023-10-23 16:07:26 +0300

    added comma after a last field

--- world_books/catalog/models.py
+++ world_books/catalog/models.py
@@ -6,7 +6,7 @@ class Genre(models.Model):
     name = models.CharField(
         max_length=200,
         help_text='Введите жанр книги',
-        verbose_name='Жанр книги'
+        verbose_name='Жанр книги',
     )
 
     def __str__(self):
@@ -17,7 +17,7 @@ class Language(models.Model):
     name = models.CharField(
         max_length=20,
         help_text='Введите язык книги',
-        verbose_name='Язык книги'
+        verbose_name='Язык книги',
     )
 
     def __str__(self):
@@ -28,7 +28,7 @@ class Publisher(models.Model):
     name = models.CharField(
         max_length=20,
         help_text='Введите наименование издательства',
-        verbose_name='Издательство'
+        verbose_name='Издательство',
     )
 
     def __str__(self):
@@ -39,29 +39,29 @@ class Author(models.Model):
     first_name = models.CharField(
         max_length=100,
         help_text='Введите имя',
-        verbose_name='Имя автора'
+        verbose_name='Имя автора',
     )
     last_name = models.CharField(
         max_length=100,
         help_text='Введите фамилию',
-        verbose_name='Фамилия автора'
+        verbose_name='Фамилия автора',
     )
     date_birth = models.DateField(
         help_text='Введите дату рождения',
         verbose_name='Дата рождения',
         null=True,
-        blank=True
+        blank=True,
     )
     about = models.TextField(
         help_text='Введите сведения об авторе',
-        verbose_name='Сведения об авторе'
+        verbose_name='Сведения об авторе',
     )
     photo = models.ImageField(
         upload_to='images',
         help_text='Загрузите фото автора',
         verbose_name='Фото автора',
         null=True,
-        blank=True
+        blank=True,
     )
 
     def __str__(self):
@@ -72,38 +72,38 @@ class Book(models.Model):
     title = models.CharField(
         max_length=200,
         help_text='Введите название книги',
-        verbose_name='Название книги'
+        verbose_name='Название книги',
     )
     genre = models.ForeignKey(
         Genre,
         on_delete=models.CASCADE,
         help_text='Выберите жанр для книги',
         verbose_name='Жанр книги',
-        null=True
+        null=True,
     )
     language = models.ForeignKey(
         Language,
         on_delete=models.CASCADE,
         help_text='Выберите язык книги',
         verbose_name='Язык книги',
-        null=True
+        null=True,
     )
     publisher = models.ForeignKey(
         Publisher,
         on_delete=models.CASCADE,
         help_text='Выберите издательство',
         verbose_name='Издательство',
-        null=True
+        null=True,
     )
     year = models.CharField(
         max_length=4,
         help_text='Введите год издания',
-        verbose_name='Год издания'
+        verbose_name='Год издания',
     )
     author = models.ManyToManyField(
         Author,
         help_text='Выберите автора (авторов) книги',
-        verbose_name='Автор (авторы) книги'
+        verbose_name='Автор (авторы) книги',
     )
     summary = models.TextField(
         max_length=1000,
@@ -113,18 +113,18 @@ class Book(models.Model):
     isbn = models.CharField(
         max_length=13,
         help_text='Должно содержать 13 символов',
-        verbose_name='ISBN книги'
+        verbose_name='ISBN книги',
     )
     price = models.DecimalField(
         decimal_places=2,
         max_digits=7,
         help_text='Введите цену книги',
-        verbose_name='Цена (руб.)'
+        verbose_name='Цена (руб.)',
     )
     photo = models.ImageField(
         upload_to='images',
         help_text='Загрузите изображение обложки',
-        verbose_name='Изображение обложки'
+        verbose_name='Изображение обложки',
     )
 
     def __str__(self):
@@ -139,7 +139,7 @@ class Status(models.Model):
     name = models.CharField(
         max_length=20,
         help_text='Введите статус экземпляра книги',
-        verbose_name='Статус экземпляра книги'
+        verbose_name='Статус экземпляра книги',
     )
 
     def __str__(self):
@@ -176,4 +176,3 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f'{self.inventory_number} {self.book} {self.status}'
-
