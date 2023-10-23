20910e3f7f8a54c13264bdbb1bbd3dc5c9fb68ef
Comandosss <cotum@mail.ru>
2023-10-23 20:56:15 +0300

    registered ModelAdmin

--- world_books/catalog/admin.py
+++ world_books/catalog/admin.py
@@ -4,10 +4,22 @@ from catalog.models import (Author, Book, Genre, Language,
                             Publisher, Status, BookInstance)
 
 
-admin.site.register(Author)
-admin.site.register(Book)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_birth', 'photo')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'language', 'display_author')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Publisher)
 admin.site.register(Status)
-admin.site.register(BookInstance)
