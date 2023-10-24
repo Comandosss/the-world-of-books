a05c06e7cdd17f27111cbaf4efe9e73959d23be4
Comandosss <cotum@mail.ru>
2023-10-24 18:51:19 +0300

    renamed field, fixed indents for PEP8, deleted blank strings

--- world_books/catalog/admin.py
+++ world_books/catalog/admin.py
@@ -12,7 +12,7 @@ class AuthorAdmin(admin.ModelAdmin):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'genre', 'language', 'display_author')
+    list_display = ('title', 'genre', 'language', 'show_authors')
     list_filter = ('genre', 'author')
 
 
@@ -20,15 +20,18 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('book', 'status')
     fieldsets = (
-        ('Экземпляр книги', {
-            'fields': ('book', 'inventory_number'),
+        (
+            'Экземпляр книги', {
+                'fields': ('book', 'inventory_number'),
             },
         ),
-        ('Статус и окончание его действия', {
-            'fields': ('status', 'due_back'),
+        (
+            'Статус и окончание его действия', {
+                'fields': ('status', 'due_back'),
             },
         ),
     )
+
 
 admin.site.register(Genre)
 admin.site.register(Language)
