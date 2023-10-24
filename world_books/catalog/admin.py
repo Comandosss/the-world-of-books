007855d14351fdb124003cae79ad439355511d6b
Comandosss <cotum@mail.ru>
2023-10-24 11:41:21 +0300

    added fields, list_filter, fieldsets

--- world_books/catalog/admin.py
+++ world_books/catalog/admin.py
@@ -7,16 +7,28 @@ from catalog.models import (Author, Book, Genre, Language,
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_birth', 'photo')
+    fields = ['last_name', 'first_name', ('date_birth', 'photo')]
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', 'display_author')
+    list_filter = ('genre', 'author')
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('book', 'status')
+    fieldsets = (
+        ('Экземпляр книги', {
+            'fields': ('book', 'inventory_number'),
+            },
+        ),
+        ('Статус и окончание его действия', {
+            'fields': ('status', 'due_back'),
+            },
+        ),
+    )
 
 
 admin.site.register(Genre)
