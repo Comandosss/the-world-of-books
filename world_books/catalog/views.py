97f1c8a57c4a72e731f5562b9309937048eecae2
Comandosss <cotum@mail.ru>
2023-10-29 15:21:37 +0300

    amended into index

--- world_books/catalog/views.py
+++ world_books/catalog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
-from catalog.services import fetch_books_information_from_db
+from catalog.services import fetch_information_books_from_db
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    context = fetch_books_information_from_db()
+    context = fetch_information_books_from_db()
     return render(request, 'catalog/index.html', context)
