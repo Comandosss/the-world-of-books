38947e7983e9be474f93c47432099d2fb15f1be5
Comandosss <cotum@mail.ru>
2023-10-29 15:16:18 +0300

    renamed func, changed method for fetching 3 fields instead of all fields

--- world_books/catalog/services.py
+++ world_books/catalog/services.py
@@ -1,14 +1,15 @@
 from catalog.models import Book, BookInstance, Author
 
 
-def fetch_books_information_from_db():
+def fetch_information_books_from_db():
     """Чтение из БД данных о книгах, их количестве в наличии
     и количестве авторов
     """
 
     text_head = 'На нашем сайте вы можете получить книги в электронном виде'
 
-    books = Book.objects.all()
+    books = Book.objects.only('title', 'price', 'photo')
+
     number_books = Book.objects.count()
 
     number_instances_available = (BookInstance.objects
@@ -22,7 +23,7 @@ def fetch_books_information_from_db():
         'books': books,
         'number_books': number_books,
         'number_instances_available': number_instances_available,
-        'number_authors': number_authors
+        'number_authors': number_authors,
     }
 
     return context
