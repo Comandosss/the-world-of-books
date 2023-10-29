7a4a64138deb91105ae5eebe87be5048f74662c4
Comandosss <cotum@mail.ru>
2023-10-29 17:33:11 +0300

    added text_head into context

--- world_books/catalog/views.py
+++ world_books/catalog/views.py
@@ -6,4 +6,9 @@ from catalog.services import fetch_information_books_from_db
 
 def index(request: HttpRequest) -> HttpResponse:
     context = fetch_information_books_from_db()
+    context.update({
+        'text_head':
+            'На нашем сайте вы можете получить книги в электронном виде',
+        },
+    )
     return render(request, 'catalog/index.html', context)
