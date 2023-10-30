5d5c3a467901200bea3b9428e65edd2c57c6c2ff
Comandosss <cotum@mail.ru>
2023-10-30 17:29:11 +0300

    created some funcs for viewing list of books and detail information about the book

--- world_books/catalog/views.py
+++ world_books/catalog/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
-from catalog.services import fetch_information_books_from_db
+from catalog.services import (fetch_books_statistics,
+                              retrieve_book_detail_information)
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    context = fetch_information_books_from_db()
+    context = fetch_books_statistics()
     context.update({
         'text_head':
             'На нашем сайте вы можете получить книги в электронном виде',
         },
     )
     return render(request, 'catalog/index.html', context)
+
+
+def books_list(request: HttpRequest) -> HttpResponse:
+    context = fetch_books_statistics()
+    return render(request, 'catalog/books_list.html', context)
+
+
+def book_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    context = retrieve_book_detail_information(pk)
+    return render(request, 'catalog/book_detail.html', context)
