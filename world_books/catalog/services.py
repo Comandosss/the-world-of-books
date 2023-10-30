caf458e6f08634249ba3bf1c38116c5abb37b892
Comandosss <cotum@mail.ru>
2023-10-30 17:26:26 +0300

    renamed function, added function for receiving detail information about book

--- world_books/catalog/services.py
+++ world_books/catalog/services.py
@@ -1,12 +1,8 @@
 from catalog.models import Book, BookInstance, Author
 
 
-def fetch_information_books_from_db():
-    """Чтение из БД данных о книгах, их количестве в наличии
-    и количестве авторов
-    """
-
-    books = Book.objects.only('title', 'price', 'photo')
+def fetch_books_statistics():
+    books = Book.objects.all()
 
     number_books = Book.objects.count()
 
@@ -21,6 +17,22 @@ def fetch_information_books_from_db():
         'number_books': number_books,
         'number_instances_available': number_instances_available,
         'number_authors': number_authors,
+    }
+
+    return context
+
+
+def retrieve_book_detail_information(pk):
+    book = Book.objects.prefetch_related('bookinstance_set__status').get(pk=pk)
+
+    author_book = book.author.only('first_name', 'last_name')
+
+    instances = book.bookinstance_set.all()
+
+    context = {
+        'book': book,
+        'instances': instances,
+        'author_book': author_book,
     }
 
     return context
