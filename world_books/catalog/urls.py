281c16e23615133a77ed7be1aac34a684f03df3d
Comandosss <cotum@mail.ru>
2023-10-30 17:27:01 +0300

    added paths

--- world_books/catalog/urls.py
+++ world_books/catalog/urls.py
@@ -5,4 +5,6 @@ from catalog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/<int:pk>/', views.book_detail, name='details'),
+    path('books/', views.books_list, name='books'),
 ]
