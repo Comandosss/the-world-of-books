5e2a63d012b407f868d806c087df5166f3728157
Comandosss <cotum@mail.ru>
2023-11-01 19:38:23 +0300

    deleted text_head, added pagination

--- world_books/catalog/views.py
+++ world_books/catalog/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 from catalog.services import (fetch_books_statistics,
-                              retrieve_book_detail_information)
+                              retrieve_book_detail_information,
+                              paginate)
 
 
 def index(request: HttpRequest) -> HttpResponse:
     context = fetch_books_statistics()
-    context.update({
-        'text_head':
-            'На нашем сайте вы можете получить книги в электронном виде',
-        },
-    )
     return render(request, 'catalog/index.html', context)
 
 
 def books_list(request: HttpRequest) -> HttpResponse:
     context = fetch_books_statistics()
+    books = context['books']
+
+    page_number = request.GET.get('page', 1)
+    page = paginate(books, page_number)
+
+    context.update({'page': page, 'books': page.object_list})
+
     return render(request, 'catalog/books_list.html', context)
 
 
